af5c620af6ebbff395ec0d1175573645cbd8204e
Mariam-Elkhelawy <mariamelkhelawy2001@gmail.com>
2024-08-11 22:14:45 +0300

    learn image view

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                      Toast.LENGTH_SHORT).show();
             }
         });
+        ImageView imageView =findViewById(R.id.myImage);
 
     }
 }
